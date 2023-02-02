356e38baffc4e066ca4a8cdc3560323b3d36a7be
Oybek Berkinov <oybekberkinov3@gmail.com>
2023-02-02 22:49:40 +0500

    updated and its works

--- Bot.py
+++ Bot.py
@@ -1,17 +1,18 @@
+import gspread
 import logging
 import Config
-import sqlite3
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-conn = sqlite3.connect("C:/Users/oybek/OneDrive/Desktop/db/Medical.db", check_same_thread=False)
-cursor = conn.cursor()
+sa = gspread.service_account(filename="acount.json")
+sh = sa.open("Medicaltgbot")
 
-def db_table_val(user_id: int, user_name: str, name: str, number: int, problem: str):
-	cursor.execute('INSERT INTO reCall (user_id, user_name, name, number, problem) VALUES (?, ?, ?, ?, ?)', (user_id, user_name, name, number,problem))
-	conn.commit()
+global row
+row = 2
+wks = sh.worksheet("CallBack")
+
 
 # Тут токен крч из конфига
 API_TOKEN = Config.TOKEN
@@ -30,51 +31,46 @@ class Form(StatesGroup):
     start = State()
     end = State()
 
-
 # улавливает команду старт
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     await Form.name.set()
+    global userId
+    userId = message.from_user.id
     await message.answer("Как вас зовут?",reply_markup=types.ReplyKeyboardRemove())
-    global us_id
-    global us_name
-    us_id = message.from_user.id
-    us_name = message.from_user.first_name
 
 @dp.message_handler(text="повторная запись")
 async def cmd_start(message: types.Message):
-    global us_id
-    global us_name
     await Form.name.set()
+    global userId
+    userId = message.from_user.id
     await message.answer("Как вас зовут?",reply_markup=types.ReplyKeyboardRemove())
-    us_id = message.from_user.id
-    us_name = message.from_user.first_name
 
 
 @dp.message_handler(state=Form.name)
 async def process_name(message: types.Message, state: FSMContext):
     await state.finish()
-    global uName
-    uName = message.text
+    global userName
+    userName = message.text
     await message.reply(f"Здравствуйте, {message.text}", reply_markup=types.ReplyKeyboardRemove())
     await Form.number.set()
     await message.reply(f"{message.text} , отправьте свой номер телефона с кодом")
 
+
 @dp.message_handler(state=Form.number)
 async def process_name(message: types.Message, state: FSMContext):
     await state.finish()
-    global uPhone
-    uPhone = message.text
+    global userPhone
+    userPhone = message.text
     await Form.problem.set()
     await message.reply("Опишите свою проблему :")
-
 
 
 @dp.message_handler(state=Form.problem)
 async def process_name(message: types.Message, state: FSMContext):
     await state.finish()
-    global uProblem
-    uProblem = message.text
+    global userProblem
+    userProblem = message.text
     kb = [[types.KeyboardButton(text="пропустить")]]
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=kb,
@@ -88,7 +84,11 @@ async def process_name(message: types.Message, state: FSMContext):
 @dp.message_handler(state=Form.photo)
 async def process_name(message: types.Message, state: FSMContext):
     await state.finish()
-    db_table_val(user_id=us_id, user_name=us_name, name=uName, number=uPhone, problem=uProblem)
+    wks.update('A' + str(row), userId)
+    wks.update('B' + str(row), userName)
+    wks.update('C' + str(row), userPhone)
+    wks.update('D' + str(row), userProblem)
+    wks.update('E' + str(row), message.text)
     await Form.end.set()
     await message.reply("Спасибо с вами обязательно свяжутся",reply_markup=types.ReplyKeyboardRemove())
 
@@ -97,12 +97,18 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
         return
+    wks.update('A'+str(row), userId)
+    wks.update('B'+str(row), userName)
+    wks.update('C' + str(row), userPhone)
+    wks.update('D' + str(row), userProblem)
+    wks.update('E' + str(row), message.text)
     await state.finish()
     await Form.end.set()
     await message.reply('Спасибо с вами обязательно свяжутся',reply_markup=types.ReplyKeyboardRemove())
 
 @dp.message_handler(state=Form.end)
 async def cancel_handler(message: types.Message,state : FSMContext):
+    global row
     await state.finish()
     kb = [[types.KeyboardButton(text="повторная запись")]]
     keyboard = types.ReplyKeyboardMarkup(
@@ -110,6 +116,7 @@ async def cancel_handler(message: types.Message,state : FSMContext):
         resize_keyboard=True,
         input_field_placeholder="или напишите повторная запись"
     )
+    row += 1
     await message.answer("Желаете еще раз записаться ?", reply_markup=keyboard)
 
 
